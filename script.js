af86f0c2ab2b72a97cb4cdd37be6586b5843613f
Scott Wright <scott.wright.uk@gmail.com>
2023-05-15 11:20:33 +0100

    feat: add book addition function
    This commit adds the functionality to add new books to the library by listening for form submissions and updating the array.
    The function selects the form element with the class .add-book-form and adds an event listener to the form's submit event.
    When the form is submitted, the default form submission behavior is prevented using .preventDefault();
    The input values from the form fields (title, author, pages, and read status) are retrieved using getElementById, and a new book object is created with the values.
    The new book is then pushed to the array.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const myLibrary = [];
+
+// -- BOOK CONSTRUCTOR -- //
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -9,3 +13,25 @@ function Book(title, author, pages, read) {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
   };
 }
+
+// -- ADD BOOK FUNCTION -- //
+
+function addBookToLibrary() {
+  const form = document.querySelector('.add-book-form');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').value;
+
+    const addNewBook = new Book(title, author, pages, read);
+    myLibrary.push(addNewBook);
+  });
+}
+
+console.log(myLibrary);
+
+addBookToLibrary();
